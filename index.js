11a4d1e13043852931c26472c6ca964a3fc3c3a4
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-04-18 21:40:30 -0700

    Uses advanced techniques for a record-oriented employee punch card system.

--- index.js
+++ index.js
@@ -1,4 +1,62 @@
 /* Your Code Here */
+function createEmployeeRecord(employeeArray) {
+  const [firstName, familyName, title, payPerHour] = employeeArray;
+  const employeeRecord = {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+
+  return employeeRecord;
+}
+
+function createEmployeeRecords(arrayOfEmployees) {
+  const arrayOfEmployeeRecords = arrayOfEmployees.map(createEmployeeRecord);
+  return arrayOfEmployeeRecords;
+}
+
+function createTimeInEvent(dateTime) {
+  const [date, hour] = dateTime.split(" ");
+  const timeInEvent = {
+    type: "TimeIn",
+    date: date,
+    hour: parseInt(hour),
+  };
+  this.timeInEvents.push(timeInEvent);
+  return this;
+}
+
+function createTimeOutEvent(dateTime) {
+  const [date, hour] = dateTime.split(" ");
+  const timeOutEvent = {
+    type: "TimeOut",
+    date: date,
+    hour: parseInt(hour),
+  };
+  this.timeOutEvents.push(timeOutEvent);
+  return this;
+}
+
+function hoursWorkedOnDate(date) {
+  const matchingStart = this.timeInEvents.find(
+    (timeInEvent) => timeInEvent.date === date
+  );
+  const matchingStop = this.timeOutEvents.find(
+    (timeOutEvent) => timeOutEvent.date === date
+  );
+  const hoursWorkedInteger = (matchingStop.hour - matchingStart.hour) / 100;
+
+  return hoursWorkedInteger;
+}
+
+function wagesEarnedOnDate(date) {
+  const hoursWorked = hoursWorkedOnDate.call(this, date);
+  const payOwed = hoursWorked * this.payPerHour;
+  return payOwed;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -10,14 +68,30 @@
  */
 
 const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+  const eligibleDates = this.timeInEvents.map(function (e) {
+    return e.date;
+  });
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+  const payable = eligibleDates.reduce(
+    function (memo, d) {
+      return memo + wagesEarnedOnDate.call(this, d);
+    }.bind(this),
+    0
+  ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
+  return payable;
+};
+
+function findEmployeeByFirstName(arrayOfEmployeeRecords, firstName) {
+  const matchingRecord = arrayOfEmployeeRecords.find(
+    (employeeRecord) => employeeRecord.firstName === firstName
+  );
+  return matchingRecord;
 }
 
+function calculatePayroll(arrayOfEmployeeRecords) {
+  const sumOfPayForAll = arrayOfEmployeeRecords
+    .map((employeeRecord) => allWagesFor.call(employeeRecord))
+    .reduce((payRoll, currentWage) => payRoll + currentWage);
+  return sumOfPayForAll;
+}
